7136e6dbccdabede96946cd126ee53416ce4d226
lonelybuddy <henryzheng92@gmail.com>
2022-02-07 15:26:35 -0600

    added GSearchConextProvider to fetch data from API

--- src/contexts/GSearchContextProvider.js
+++ src/contexts/GSearchContextProvider.js
@@ -1,5 +1,4 @@
-import React, { createContext, useState } from 'react';
-import { Children } from 'react/cjs/react.production.min';
+import React, { createContext, useContext, useState } from 'react';
 
 const GSearchContext = createContext();
 const API_URL = 'https://google-search3.p.rapidapi.com/api/v1';
@@ -9,7 +8,30 @@ const GSearchContextProvider = ({ children }) => {
     const [loading, setLoading] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
 
-    return <GSearchContext.Provider value={{}}>{children}</GSearchContext.Provider>;
+    const getResultsByType = async (type) => {
+        setLoading(true);
+
+        try {
+            const response = await fetch(`${API_URL}${type}`, {
+                method: 'GET',
+                headers: {
+                    'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+                    'x-rapidapi-key': process.env.React_APP_API_KEY,
+                },
+            });
+            const data = await response.json();
+
+            setResults(data);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
+    };
+
+    const contextValue = { getResultsByType, results, searchTerm, setSearchTerm, loading };
+
+    return <GSearchContext.Provider value={contextValue}>{children}</GSearchContext.Provider>;
 };
 
-export default GSearchContextProvider;
+export const useGSearchContext = () => useContext(GSearchContextProvider);
